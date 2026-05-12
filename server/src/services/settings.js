d7e93ca728a1a8f037bd3ade8af58a841cad3f49
Jonathan Desrosiers <359867+desrosj@users.noreply.github.com>
2026-05-12 12:07:17 -0400

    Change the default obfuscated string.

--- server/src/services/settings.js
+++ server/src/services/settings.js
@@ -3,7 +3,7 @@ import { settingsQueries } from '../db/queries.js';
 /**
  * Sentinel value to indicate API key is set without exposing the actual value
  */
-export const API_KEY_SENTINEL = '***SET***';
+export const API_KEY_SENTINEL = '********';
 
 /**
  * Masks the LLM API key in settings for safe transmission to client
